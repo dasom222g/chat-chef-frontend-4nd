be064547eca9524f869541ee981bb0ecfc8ee944
dasom222g <dasom228@gmail.com>
2026-02-26 13:48:41 +0900

    Feat: state data send

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -5,7 +5,7 @@ import AddButton from "../components/AddButton";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
-const Info = () => {
+const Info = ({ sendIngredientList }) => {
   // logic
   const history = useNavigate();
 
@@ -32,7 +32,15 @@ const Info = () => {
     );
   };
 
+  const handleDelete = (selectedId) => {
+    const filteredList = ingredientList.filter(
+      (item) => item.id !== selectedId,
+    );
+    setIngredientList(filteredList);
+  };
+
   const handleNext = () => {
+    sendIngredientList(ingredientList);
     // 미션: chat페이지로 이동되게 기능 구현
     history("/chat");
   };
@@ -50,9 +58,9 @@ const Info = () => {
   // }, []);
 
   // 3. 특정 state가 변경이 일어났을때 실행
-  // useEffect(() => {
-  //   console.log("ingredientList", ingredientList);
-  // }, [ingredientList]);
+  useEffect(() => {
+    console.log("ingredientList", ingredientList);
+  }, [ingredientList]);
 
   // view
   return (
@@ -80,6 +88,7 @@ const Info = () => {
                   key={item.id}
                   content={item}
                   onChange={handleChange}
+                  onDelete={handleDelete}
                 />
               ))}
             </div>
